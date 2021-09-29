64de09788cdecaf33f02f8fc6c13e3a049a7cb19
VadDoc <vadprudnikov@gmail.com>
2021-09-29 19:13:43 +0300

    Updates

---
+++ precache-manifest.74da7327f1a82a62cbb209be36df2915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fef936874b43706b421ba2b967676db",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "7f249907c50a577e9b1f",
+    "url": "/Todo-List/static/css/main.306aea59.chunk.css"
+  },
+  {
+    "revision": "73c477a85ea732e5e687",
+    "url": "/Todo-List/static/js/2.6a57f15e.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Todo-List/static/js/2.6a57f15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f249907c50a577e9b1f",
+    "url": "/Todo-List/static/js/main.fdae10dc.chunk.js"
+  },
+  {
+    "revision": "dc6359743acab76c1270",
+    "url": "/Todo-List/static/js/runtime-main.894748b6.js"
+  }
+]);
